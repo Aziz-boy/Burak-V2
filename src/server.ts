f0d50499d78534a070de8377675d8370aeb1421a
Aziz-boy <shavgoniaziz@gmail.com>
2025-04-24 15:45:16 +0500

    connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,14 @@
 import dotenv from "dotenv"; //module.js
 dotenv.config(); 
+
+import mongoose from "mongoose"; 
+
+mongoose
+        .connect(process.env.MONGO_URL as string, {})
+        .then((data) => {
+            console.log("MongoDB connection succeded: ");
+            const PORT = process.env.PORT || 3003;
+        })
+        .catch((err) => {
+            console.log("ERROR on connection MongoDB: ", err);
+        });
