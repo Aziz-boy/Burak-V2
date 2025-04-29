696cc8600ffc3fb655b2ebc5232e83e90e61c002
Aziz-boy <shavgoniaziz@gmail.com>
2025-04-29 08:25:34 +0500

    feat: create routing and member controllers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import path from 'path';
+import router from './router';
+
+
 /** 1- ENTRANCE **/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,5 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4- ROUTERS **/
+app.use("/", router); //Middleware Design Pattern
+
 
 export default app;
